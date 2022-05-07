aa95f773fc0ddbc53383167e142fab841913a050
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2022-05-07 13:25:30 +0530

    Removed footer

--- src/Components/Footer.js/Footer.js
+++ src/Components/Footer.js/Footer.js
@@ -6,11 +6,6 @@ export default function Footer(props) {
       <Link to="/">
         <img src={Logo} alt="Logo" className="max-h-[250px] " />
       </Link>
-      <div className="text-left mt-10">
-          <h1 className="text-2xl font-family-titan-one">Creators: </h1>
-          <p className="text-md font-family-titan-one">Abhinandan Sharma (<a href="https://github.com/abhi-824" className="hover:text-gray-400 duration-300" target="_blank" rel="noopener noreferrer">GitHub</a>)</p>
-          <p className="text-md font-family-titan-one">Aakash Kumar Singh (<a href="https://github.com/abhi-824" className="hover:text-gray-400 duration-300" target="_blank" rel="noopener noreferrer">GitHub</a>)</p>
-      </div>
     </div>
   );
 }
